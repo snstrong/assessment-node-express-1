df5c40e258482630fc7822d921d7d25cfd4782bb
Sara Strong <sara.n.strong@gmail.com>
2021-02-02 16:10:34 -0500

    Broken app fixed, issues documented

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,49 @@
 const express = require("express");
-let axios = require("axios");
-var app = express();
+const axios = require("axios");
+const ExpressError = require("./expressError");
 
-app.post("/", function (req, res, next) {
+const app = express();
+
+app.use(express.json());
+
+app.post("/", async function (req, res, next) {
+  // Accepts a JSON request, req, containing an array of GitHub usernames,
+  // e.g, {"developers": ["elie", "joelburton"]}
+  // Retrieves GitHub data about users in req
+  // Returns JSON response, res, containing each user's name and bio as objects in an array
   try {
-    let results = req.body.developers.map(async (d) => {
-      return await axios.get(`https://api.github.com/users/${d}`);
+    let promisedDevs = [];
+    for (let dev of req.body.developers) {
+      if (!dev) {
+        throw new ExpressError("Username is required", 400);
+      }
+      let devInfo = await axios.get(`https://api.github.com/users/${dev}`);
+      promisedDevs.push(devInfo);
+    }
+    let devs = await Promise.all(promisedDevs);
+    let devsResponse = devs.map((dev) => {
+      return { name: dev.data.name, bio: dev.data.bio };
     });
-    let out = results.map((r) => ({ name: r.data.name, bio: r.data.bio }));
-
-    return res.send(JSON.stringify(out));
-  } catch {
+    return res.json(devsResponse);
+  } catch (err) {
     next(err);
   }
 });
 
-app.listen(3000);
+/** 404 handler */
+
+app.use(function (req, res, next) {
+  return new ExpressError("Not Found", 404);
+});
+
+/** General error handler */
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
+
+module.exports = app;
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,11 +1,15 @@
 # Broken App Issues
 
-- Variable declaration: better to use "const" than "var" or "let" when requiring a module, and to be consistent
+- Variable declaration: better to use "const" than "var" or "let" when requiring a module, and to be consistent; updated accordingly
 
-- Error handling: `catch` not given any parameters, `next` being used improperly
+- Need to use express.json for JSON post request; added it
 
-- Need to use express.json for JSON post request
+- Made callback for the route itself an async function, which will allow us to successfully await the resolution of the Promise returned from axios when we attempt to retreive the requested data
 
-- `return await axios.get()` is incorrect
+- Error handling bugs: `catch` not given any parameters, `next` undefined
 
-- Callback for this route should be an async function, which will allow us to await the resolution of the Promise returned from axios when we attempt to retreive the requested data
+- Created ExpressError class (in separate file for the sake of modularity) and added error handlers to app, which also fixed the aforementioned error handling bugs
+
+- Separated out the server logic into its own file to make the application more modular and scalable, and added a `console.log` to indicate when server is running
+
+- Added documentation
